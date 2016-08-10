5b851fac48900050cabd2d0f81e3ea3c4e8e37a7
Fernando Jose <delgadof@seattleu.edu>
2016-08-09 17:43:07 -0700

    Create files

---
+++ madlibs.js
@@ -0,0 +1,84 @@
+// Place your Mad Libs code here.
+
+/*
+---- Start of Variables ----
+Create the following variables:
+    - Two for funny words
+    - Six for adjective
+    - Nine for nouns
+    - One for number
+    - One for verb
+    - One for male name
+    - Three for verb ending with ed
+    - One for Place
+
+---- End of Variables ----
+*/
+
+// Mad Libs: Make it work displaying it on your browser's console.
+// Hakuna Matata - The Lion King -  Elton John (music) and Tim Rice (lyrics).
+
+/*
+---- Start of Mad Libs ----
+
+FUNNYWORD FUNNYWORD! What a(n) ADJECTIVE phrase
+
+FUNNYWORD FUNNYWORD! Ain`t no passing craze
+
+It means no NOUN for the rest of your NOUN
+
+It`s our ADJECTIVE philosophy
+
+FUNNYWORD FUNNYWORD!
+
+FUNNYWORD FUNNYWORD?
+
+Yeah. It`s our NOUN!
+
+What`s a NOUN?
+
+Nothing. What`s a-NOUN with you?
+
+Those NUMBER words will VERB all your problems
+
+That`s right. Take MALENAME here
+Why, when he was a ADJECTIVE NOUN...
+
+When I was a ADJECTIVE NOUN
+
+Very ADJECTIVE
+
+Thanks
+
+He found his NOUN VERBWITHED a certain appeal
+He could clear the/a PLACE after every NOUN
+
+I`m a ADJECTIVE soul though I seem ADJECTIVE
+And it VERBWITHED that my NOUN never stood downwind
+And oh, the shame - He was ashamed
+Thought of changin` my NOUN What`s in a NOUN?
+And I got VERBWITHED How did ya feel?
+Everytime that I...
+
+Hey! MALENAME! Not in front of the NOUN!
+Oh. Sorry
+
+FUNNYWORD FUNNYWORD! What a ADJECTIVE phrase
+FUNNYWORD FUNNYWORD! Ain`t no passing craze
+
+It means no NOUN for the rest of your NOUN
+
+It`s our ADJECTIVE philosophy
+FUNNYWORD FUNNYWORD!
+FUNNYWORD FUNNYWORD! FUNNYWORD FUNNYWORD!
+FUNNYWORD FUNNYWORD! FUNNYWORD FUNNYWORD!
+FUNNYWORD FUNNYWORD! FUNNYWORD FUNNYWORD!
+FUNNYWORD FUNNYWORD! FUNNYWORD FUNNYWORD
+
+It means no NOUN for the rest of your NOUN
+
+It`s our ADJECTIVE philosophy
+FUNNYWORD FUNNYWORD!
+
+---- End of Mad Libs ----
+*/
